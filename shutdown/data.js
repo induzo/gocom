ecf3375bbc9b1b67b3b67402dc754cd1b32bd9c3
github-action-benchmark <github@users.noreply.github.com>
2023-03-31 03:46:45 +0000

    add Benchmark (go) benchmark result for 64eb43b5e4e7dbbf8f586a1464327ac7890baaf6

---
+++ shutdown/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1680234405769,
+  "repoUrl": "https://github.com/induzo/gocom",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "64eb43b5e4e7dbbf8f586a1464327ac7890baaf6",
+          "message": "docs(changelog): update CHANGELOG.md for shutdown/v1.0.0",
+          "timestamp": "2023-03-31T11:45:44+08:00",
+          "tree_id": "17b2aa4aae0191a04ec3a60ff1129f2aea1fcd3b",
+          "url": "https://github.com/induzo/gocom/commit/64eb43b5e4e7dbbf8f586a1464327ac7890baaf6"
+        },
+        "date": 1680234405096,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkShutdown",
+            "value": 12472817,
+            "unit": "ns/op\t    1694 B/op\t      34 allocs/op",
+            "extra": "94 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
