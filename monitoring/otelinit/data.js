6319fb949df4b628502552126b9003f4e81cbd9a
github-action-benchmark <github@users.noreply.github.com>
2023-03-31 03:47:41 +0000

    add Benchmark (go) benchmark result for 64eb43b5e4e7dbbf8f586a1464327ac7890baaf6

--- monitoring/otelinit/data.js
+++ monitoring/otelinit/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680232567861,
+  "lastUpdate": 1680234461624,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 42187,
             "unit": "ns/op\t   48699 B/op\t      56 allocs/op",
             "extra": "32035 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "64eb43b5e4e7dbbf8f586a1464327ac7890baaf6",
+          "message": "docs(changelog): update CHANGELOG.md for shutdown/v1.0.0",
+          "timestamp": "2023-03-31T11:45:44+08:00",
+          "tree_id": "17b2aa4aae0191a04ec3a60ff1129f2aea1fcd3b",
+          "url": "https://github.com/induzo/gocom/commit/64eb43b5e4e7dbbf8f586a1464327ac7890baaf6"
+        },
+        "date": 1680234460901,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkInitProvider",
+            "value": 7655,
+            "unit": "ns/op\t    1949 B/op\t      47 allocs/op",
+            "extra": "159957 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkInitProvider",
+            "value": 44957,
+            "unit": "ns/op\t   48697 B/op\t      56 allocs/op",
+            "extra": "30255 times\n2 procs"
           }
         ]
       }
