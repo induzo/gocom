bb930a3d78298e0ebcf2a724ce4e1ac1a35c9b04
github-action-benchmark <github@users.noreply.github.com>
2023-03-31 03:16:07 +0000

    add Benchmark (go) benchmark result for b57a2784c0e6743807d231a37e2cb7893c649abd

---
+++ monitoring/otelinit/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1680232567861,
+  "repoUrl": "https://github.com/induzo/gocom",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "b57a2784c0e6743807d231a37e2cb7893c649abd",
+          "message": "ci: add codecov",
+          "timestamp": "2023-03-31T11:14:32+08:00",
+          "tree_id": "c8fc4a926d89c09fc7eca37d0eca1a8b7fa51e62",
+          "url": "https://github.com/induzo/gocom/commit/b57a2784c0e6743807d231a37e2cb7893c649abd"
+        },
+        "date": 1680232567174,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkInitProvider",
+            "value": 5079,
+            "unit": "ns/op\t    1761 B/op\t      45 allocs/op",
+            "extra": "235774 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkInitProvider",
+            "value": 42187,
+            "unit": "ns/op\t   48699 B/op\t      56 allocs/op",
+            "extra": "32035 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
