b25d553f0f09f2b597eebc847a12c9c72f70a998
github-action-benchmark <github@users.noreply.github.com>
2023-04-01 10:07:45 +0000

    add Benchmark (go) benchmark result for 257c587ed9fe9524cace9c95690a83601644028c

--- database/pginit/data.js
+++ database/pginit/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680342375617,
+  "lastUpdate": 1680343665964,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 123857397,
             "unit": "ns/op\t 4718816 B/op\t   31610 allocs/op",
             "extra": "9 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "257c587ed9fe9524cace9c95690a83601644028c",
+          "message": "chore: work sync",
+          "timestamp": "2023-04-01T18:06:06+08:00",
+          "tree_id": "60a6e24cc82ee767c9627715ee5bd366d024fc41",
+          "url": "https://github.com/induzo/gocom/commit/257c587ed9fe9524cace9c95690a83601644028c"
+        },
+        "date": 1680343664876,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkConnPool",
+            "value": 145610227,
+            "unit": "ns/op\t 4870834 B/op\t   32585 allocs/op",
+            "extra": "7 times\n2 procs"
           }
         ]
       }
