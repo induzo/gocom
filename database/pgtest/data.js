40db67b0f1544e2f74986d852f471ba1fd9cec21
github-action-benchmark <github@users.noreply.github.com>
2023-04-01 09:15:11 +0000

    add Benchmark (go) benchmark result for e3f2e34febb98b5fc96bb393ae27a885073d1475

---
+++ database/pgtest/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1680340511787,
+  "repoUrl": "https://github.com/induzo/gocom",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "e3f2e34febb98b5fc96bb393ae27a885073d1475",
+          "message": "docs: update docs for module database/pgx-slog",
+          "timestamp": "2023-04-01T17:12:40+08:00",
+          "tree_id": "68a3012a04550f2ecdd3c1f85f91a6f6e5228864",
+          "url": "https://github.com/induzo/gocom/commit/e3f2e34febb98b5fc96bb393ae27a885073d1475"
+        },
+        "date": 1680340511331,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkNew",
+            "value": 4238578196,
+            "unit": "ns/op\t 3446952 B/op\t   22183 allocs/op",
+            "extra": "1 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
