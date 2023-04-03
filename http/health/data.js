aca779d9c8728fd859e10d77939f45c3cb91e65a
github-action-benchmark <github@users.noreply.github.com>
2023-04-03 06:59:03 +0000

    add Benchmark (go) benchmark result for b674d6feeb830a0cf6714a0df61e232642561f0e

---
+++ http/health/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1680505143875,
+  "repoUrl": "https://github.com/induzo/gocom",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "b674d6feeb830a0cf6714a0df61e232642561f0e",
+          "message": "feat: add http/health",
+          "timestamp": "2023-04-03T14:57:01+08:00",
+          "tree_id": "8cc38e2516e72915926d7113dc5ed9bc7e20e8f7",
+          "url": "https://github.com/induzo/gocom/commit/b674d6feeb830a0cf6714a0df61e232642561f0e"
+        },
+        "date": 1680505142984,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkHealth",
+            "value": 3291,
+            "unit": "ns/op\t    1048 B/op\t      17 allocs/op",
+            "extra": "335194 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
