d0487604aa52766b87a205314504f34f89783c33
github-action-benchmark <github@users.noreply.github.com>
2023-04-03 06:59:13 +0000

    add Benchmark (go) benchmark result for b674d6feeb830a0cf6714a0df61e232642561f0e

--- monitoring/otelinit/data.js
+++ monitoring/otelinit/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680341003299,
+  "lastUpdate": 1680505153449,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -144,6 +144,42 @@ window.BENCHMARK_DATA = {
             "value": 56848,
             "unit": "ns/op\t   48690 B/op\t      56 allocs/op",
             "extra": "24064 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "b674d6feeb830a0cf6714a0df61e232642561f0e",
+          "message": "feat: add http/health",
+          "timestamp": "2023-04-03T14:57:01+08:00",
+          "tree_id": "8cc38e2516e72915926d7113dc5ed9bc7e20e8f7",
+          "url": "https://github.com/induzo/gocom/commit/b674d6feeb830a0cf6714a0df61e232642561f0e"
+        },
+        "date": 1680505152706,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkInitProvider",
+            "value": 6552,
+            "unit": "ns/op\t    1909 B/op\t      47 allocs/op",
+            "extra": "163430 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkInitProvider",
+            "value": 41679,
+            "unit": "ns/op\t   48695 B/op\t      56 allocs/op",
+            "extra": "31354 times\n2 procs"
           }
         ]
       }
