260e8d3c9f39672f3835f324306dffdbe108adfa
github-action-benchmark <github@users.noreply.github.com>
2023-04-03 06:58:47 +0000

    add Benchmark (go) benchmark result for b674d6feeb830a0cf6714a0df61e232642561f0e

--- database/pginit/data.js
+++ database/pginit/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680343665964,
+  "lastUpdate": 1680505127763,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -59,6 +59,36 @@ window.BENCHMARK_DATA = {
             "name": "BenchmarkConnPool",
             "value": 145610227,
             "unit": "ns/op\t 4870834 B/op\t   32585 allocs/op",
+            "extra": "7 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "b674d6feeb830a0cf6714a0df61e232642561f0e",
+          "message": "feat: add http/health",
+          "timestamp": "2023-04-03T14:57:01+08:00",
+          "tree_id": "8cc38e2516e72915926d7113dc5ed9bc7e20e8f7",
+          "url": "https://github.com/induzo/gocom/commit/b674d6feeb830a0cf6714a0df61e232642561f0e"
+        },
+        "date": 1680505126739,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkConnPool",
+            "value": 168206025,
+            "unit": "ns/op\t 4859296 B/op\t   32537 allocs/op",
             "extra": "7 times\n2 procs"
           }
         ]
