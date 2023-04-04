a2bda4d243d762e1b87834381cc2c76ed23a8c8b
github-action-benchmark <github@users.noreply.github.com>
2023-04-04 01:39:15 +0000

    add Benchmark (go) benchmark result for 3a30ed7fcf2003e8bbd3c0452e5d6d98680b09de

--- database/pgx-slog/data.js
+++ database/pgx-slog/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680512360220,
+  "lastUpdate": 1680572355462,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -180,6 +180,42 @@ window.BENCHMARK_DATA = {
             "value": 2197,
             "unit": "ns/op\t      85 B/op\t       3 allocs/op",
             "extra": "551805 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "3a30ed7fcf2003e8bbd3c0452e5d6d98680b09de",
+          "message": "docs: readme line for new mod",
+          "timestamp": "2023-04-04T09:37:57+08:00",
+          "tree_id": "3042009af2bea2df5b5c3746685366019657691b",
+          "url": "https://github.com/induzo/gocom/commit/3a30ed7fcf2003e8bbd3c0452e5d6d98680b09de"
+        },
+        "date": 1680572354392,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkNewLogger",
+            "value": 0.4518,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLogger_Log",
+            "value": 2657,
+            "unit": "ns/op\t      85 B/op\t       3 allocs/op",
+            "extra": "464720 times\n2 procs"
           }
         ]
       }
