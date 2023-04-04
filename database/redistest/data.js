8819d49793d530e2f78ba9f71e7a77d8e38982a7
github-action-benchmark <github@users.noreply.github.com>
2023-04-04 01:44:59 +0000

    add Benchmark (go) benchmark result for bcc7774ec78d8493fbd58e64da4a499510160310

---
+++ database/redistest/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1680572698986,
+  "repoUrl": "https://github.com/induzo/gocom",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "bcc7774ec78d8493fbd58e64da4a499510160310",
+          "message": "docs(readme): update latest versions",
+          "timestamp": "2023-04-04T09:43:36+08:00",
+          "tree_id": "46664522e30d91843ccac70e51ab7a579474c650",
+          "url": "https://github.com/induzo/gocom/commit/bcc7774ec78d8493fbd58e64da4a499510160310"
+        },
+        "date": 1680572698396,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkNew",
+            "value": 1358063398,
+            "unit": "ns/op\t  384256 B/op\t    2920 allocs/op",
+            "extra": "1 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
