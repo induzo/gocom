0102e1c5d9044e2d87ee3d3d3982b5b4b6c28888
github-action-benchmark <github@users.noreply.github.com>
2023-04-10 07:52:55 +0000

    add Benchmark (go) benchmark result for abb4d866bcea67b9b3f3675b439eb35e45a8a628

--- database/redistest/data.js
+++ database/redistest/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681109313366,
+  "lastUpdate": 1681113175123,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -60,6 +60,36 @@ window.BENCHMARK_DATA = {
             "value": 1019987077,
             "unit": "ns/op\t  380072 B/op\t    2899 allocs/op",
             "extra": "1 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "abb4d866bcea67b9b3f3675b439eb35e45a8a628",
+          "message": "feat: add changelog for database/pgx-slog",
+          "timestamp": "2023-04-10T15:51:35+08:00",
+          "tree_id": "bbea04d400e6de36eed1c4b1e115ff52d0755832",
+          "url": "https://github.com/induzo/gocom/commit/abb4d866bcea67b9b3f3675b439eb35e45a8a628"
+        },
+        "date": 1681113174054,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkNew",
+            "value": 761001120,
+            "unit": "ns/op\t  269468 B/op\t    1974 allocs/op",
+            "extra": "2 times\n2 procs"
           }
         ]
       }
