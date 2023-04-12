9baacd4917413949a6b7a25a83031299f08ec838
github-action-benchmark <github@users.noreply.github.com>
2023-04-12 07:54:50 +0000

    add Benchmark (go) benchmark result for 9d0cccd53752c6559c50453b7092b02d56098eca

--- database/redistest/data.js
+++ database/redistest/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681285950295,
+  "lastUpdate": 1681286090524,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -479,6 +479,36 @@ window.BENCHMARK_DATA = {
             "name": "BenchmarkNew",
             "value": 1018002778,
             "unit": "ns/op\t  381432 B/op\t    2926 allocs/op",
+            "extra": "1 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "9d0cccd53752c6559c50453b7092b02d56098eca",
+          "message": "fix: need all depth for conv commit checks",
+          "timestamp": "2023-04-12T15:52:47+08:00",
+          "tree_id": "378564dc65e2e185c271600e8b8c8003349c7be4",
+          "url": "https://github.com/induzo/gocom/commit/9d0cccd53752c6559c50453b7092b02d56098eca"
+        },
+        "date": 1681286087973,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkNew",
+            "value": 1193441662,
+            "unit": "ns/op\t  377384 B/op\t    2905 allocs/op",
             "extra": "1 times\n2 procs"
           }
         ]
