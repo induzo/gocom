6dff4173eb91652ecf66c8400812b94cd73f02e4
github-action-benchmark <github@users.noreply.github.com>
2023-04-12 06:07:07 +0000

    add Benchmark (go) benchmark result for f8f4dcb811eed84c82b0062d70d527529c2ecee6

--- database/redisinit/data.js
+++ database/redisinit/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681119074300,
+  "lastUpdate": 1681279627377,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 79825,
             "unit": "ns/op\t     160 B/op\t       7 allocs/op",
             "extra": "14848 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "f8f4dcb811eed84c82b0062d70d527529c2ecee6",
+          "message": "feat: simplify workflows",
+          "timestamp": "2023-04-12T14:05:49+08:00",
+          "tree_id": "bf76b085fd5aa49a6cf708045c7d74ef3c5038f1",
+          "url": "https://github.com/induzo/gocom/commit/f8f4dcb811eed84c82b0062d70d527529c2ecee6"
+        },
+        "date": 1681279625907,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkXxx",
+            "value": 87988,
+            "unit": "ns/op\t     161 B/op\t       7 allocs/op",
+            "extra": "12973 times\n2 procs"
           }
         ]
       }
