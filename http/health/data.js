aee25f169eef3d7530e47ebc2b7ad075443a3670
github-action-benchmark <github@users.noreply.github.com>
2023-04-12 06:07:43 +0000

    add Benchmark (go) benchmark result for f8f4dcb811eed84c82b0062d70d527529c2ecee6

--- http/health/data.js
+++ http/health/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681118951517,
+  "lastUpdate": 1681279663685,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -450,6 +450,36 @@ window.BENCHMARK_DATA = {
             "value": 4334,
             "unit": "ns/op\t    1048 B/op\t      17 allocs/op",
             "extra": "262364 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "f8f4dcb811eed84c82b0062d70d527529c2ecee6",
+          "message": "feat: simplify workflows",
+          "timestamp": "2023-04-12T14:05:49+08:00",
+          "tree_id": "bf76b085fd5aa49a6cf708045c7d74ef3c5038f1",
+          "url": "https://github.com/induzo/gocom/commit/f8f4dcb811eed84c82b0062d70d527529c2ecee6"
+        },
+        "date": 1681279662973,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkHealth",
+            "value": 3473,
+            "unit": "ns/op\t    1048 B/op\t      17 allocs/op",
+            "extra": "331638 times\n2 procs"
           }
         ]
       }
