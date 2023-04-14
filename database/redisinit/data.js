2c5fda72462f02c26524e54e0657b79bf9ae7106
github-action-benchmark <github@users.noreply.github.com>
2023-04-14 09:35:39 +0000

    add Benchmark (go) benchmark result for e799eba6e02f1af53ad1db937804ea85e5a693f9

--- database/redisinit/data.js
+++ database/redisinit/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681286093724,
+  "lastUpdate": 1681464939976,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -180,6 +180,36 @@ window.BENCHMARK_DATA = {
             "value": 113110,
             "unit": "ns/op\t     161 B/op\t       7 allocs/op",
             "extra": "10000 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "e799eba6e02f1af53ad1db937804ea85e5a693f9",
+          "message": "docs: add badges for docs and report",
+          "timestamp": "2023-04-14T17:34:27+08:00",
+          "tree_id": "1b72c462b4f0a99ffef478aa976210e6ce6f73b3",
+          "url": "https://github.com/induzo/gocom/commit/e799eba6e02f1af53ad1db937804ea85e5a693f9"
+        },
+        "date": 1681464939162,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkXxx",
+            "value": 92927,
+            "unit": "ns/op\t     161 B/op\t       7 allocs/op",
+            "extra": "13760 times\n2 procs"
           }
         ]
       }
