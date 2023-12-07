75e0bb2f140ab200af3338a3c1ccd9e61e6b2d07
github-action-benchmark <github@users.noreply.github.com>
2023-12-07 15:32:37 +0000

    add Benchmark (go) benchmark result for 82276512673676615c5e162adfd2b18d27d54699

---
+++ http/middleware/writablecontext/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1701963157276,
+  "repoUrl": "https://github.com/induzo/gocom",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "82276512673676615c5e162adfd2b18d27d54699",
+          "message": "feat(http/middleware/writablecontext): add bench",
+          "timestamp": "2023-12-07T23:31:54+08:00",
+          "tree_id": "3056ae524c5334ad149e9ccc701510eab2ac2ef3",
+          "url": "https://github.com/induzo/gocom/commit/82276512673676615c5e162adfd2b18d27d54699"
+        },
+        "date": 1701963156593,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFromRequest - ns/op",
+            "value": 383.2,
+            "unit": "ns/op",
+            "extra": "3031910 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFromRequest - B/op",
+            "value": 659,
+            "unit": "B/op",
+            "extra": "3031910 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFromRequest - allocs/op",
+            "value": 5,
+            "unit": "allocs/op",
+            "extra": "3031910 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
