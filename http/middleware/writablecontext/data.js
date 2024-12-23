90e0ed4339e554c2d0b83804cfbec0c187f29fa1
github-action-benchmark <github@users.noreply.github.com>
2024-12-23 04:09:25 +0000

    add Benchmark (go) benchmark result for 81b4a66e1950108617daae01396f104bc98ab48b

--- http/middleware/writablecontext/data.js
+++ http/middleware/writablecontext/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729417960891,
+  "lastUpdate": 1734926965765,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -600,6 +600,54 @@ window.BENCHMARK_DATA = {
             "value": 5,
             "unit": "allocs/op",
             "extra": "2815810 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "VincentS",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "VincentS",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "81b4a66e1950108617daae01396f104bc98ab48b",
+          "message": "feat: update deps",
+          "timestamp": "2024-12-23T12:08:43+08:00",
+          "tree_id": "43af9ed585c8ed1535ace89e4697c53d718246a1",
+          "url": "https://github.com/induzo/gocom/commit/81b4a66e1950108617daae01396f104bc98ab48b"
+        },
+        "date": 1734926965279,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFromRequest",
+            "value": 407.7,
+            "unit": "ns/op\t     723 B/op\t       5 allocs/op",
+            "extra": "2801089 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFromRequest - ns/op",
+            "value": 407.7,
+            "unit": "ns/op",
+            "extra": "2801089 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFromRequest - B/op",
+            "value": 723,
+            "unit": "B/op",
+            "extra": "2801089 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFromRequest - allocs/op",
+            "value": 5,
+            "unit": "allocs/op",
+            "extra": "2801089 times\n4 procs"
           }
         ]
       }
