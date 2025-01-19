8ebced3f7cdcf9415067fef0d79ff9b78a241b31
github-action-benchmark <github@users.noreply.github.com>
2025-01-19 15:03:05 +0000

    add Benchmark (go) benchmark result for 9deec515a7803c0ea74be4763a9a8de4b84fd91a

--- database/redisinit/data.js
+++ database/redisinit/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735388781205,
+  "lastUpdate": 1737298985280,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -1698,6 +1698,54 @@ window.BENCHMARK_DATA = {
             "value": 7,
             "unit": "allocs/op",
             "extra": "10134 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9deec515a7803c0ea74be4763a9a8de4b84fd91a",
+          "message": "feat(induzo/http/middleware/idempotency): new release\n\nfirst release",
+          "timestamp": "2025-01-19T23:02:25+08:00",
+          "tree_id": "35ff75ff2da2504331c6507021b75452335df59e",
+          "url": "https://github.com/induzo/gocom/commit/9deec515a7803c0ea74be4763a9a8de4b84fd91a"
+        },
+        "date": 1737298984869,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkXxx",
+            "value": 121255,
+            "unit": "ns/op\t     177 B/op\t       7 allocs/op",
+            "extra": "9259 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkXxx - ns/op",
+            "value": 121255,
+            "unit": "ns/op",
+            "extra": "9259 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkXxx - B/op",
+            "value": 177,
+            "unit": "B/op",
+            "extra": "9259 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkXxx - allocs/op",
+            "value": 7,
+            "unit": "allocs/op",
+            "extra": "9259 times\n4 procs"
           }
         ]
       }
