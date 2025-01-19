a17a441044a7cddf235f799ee417e0c32488b967
github-action-benchmark <github@users.noreply.github.com>
2025-01-19 15:03:26 +0000

    add Benchmark (go) benchmark result for 9deec515a7803c0ea74be4763a9a8de4b84fd91a

--- database/pginit/data.js
+++ database/pginit/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735388989263,
+  "lastUpdate": 1737299006570,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -2226,6 +2226,54 @@ window.BENCHMARK_DATA = {
             "value": 308,
             "unit": "allocs/op",
             "extra": "424 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9deec515a7803c0ea74be4763a9a8de4b84fd91a",
+          "message": "feat(induzo/http/middleware/idempotency): new release\n\nfirst release",
+          "timestamp": "2025-01-19T23:02:25+08:00",
+          "tree_id": "35ff75ff2da2504331c6507021b75452335df59e",
+          "url": "https://github.com/induzo/gocom/commit/9deec515a7803c0ea74be4763a9a8de4b84fd91a"
+        },
+        "date": 1737299005626,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkConnPool",
+            "value": 2439050,
+            "unit": "ns/op\t   42892 B/op\t     308 allocs/op",
+            "extra": "492 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkConnPool - ns/op",
+            "value": 2439050,
+            "unit": "ns/op",
+            "extra": "492 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkConnPool - B/op",
+            "value": 42892,
+            "unit": "B/op",
+            "extra": "492 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkConnPool - allocs/op",
+            "value": 308,
+            "unit": "allocs/op",
+            "extra": "492 times\n4 procs"
           }
         ]
       }
