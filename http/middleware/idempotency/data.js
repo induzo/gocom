f9b3672d60ede77d6e3cdce6ae99af236de93f1a
github-action-benchmark <github@users.noreply.github.com>
2025-01-19 15:08:22 +0000

    add Benchmark (go) benchmark result for c2fc96e3612500a2bc1e90369064fdf64c354fce

--- http/middleware/idempotency/data.js
+++ http/middleware/idempotency/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737298989562,
+  "lastUpdate": 1737299302005,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -48,6 +48,54 @@ window.BENCHMARK_DATA = {
             "value": 11,
             "unit": "allocs/op",
             "extra": "321134 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "c2fc96e3612500a2bc1e90369064fdf64c354fce",
+          "message": "docs: update docs for module http/middleware/idempotency",
+          "timestamp": "2025-01-19T23:07:25+08:00",
+          "tree_id": "8b289338c1393890fcb4509507421ffa8bfc15d4",
+          "url": "https://github.com/induzo/gocom/commit/c2fc96e3612500a2bc1e90369064fdf64c354fce"
+        },
+        "date": 1737299301689,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkMiddleware",
+            "value": 3399,
+            "unit": "ns/op\t    1472 B/op\t      11 allocs/op",
+            "extra": "337650 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMiddleware - ns/op",
+            "value": 3399,
+            "unit": "ns/op",
+            "extra": "337650 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMiddleware - B/op",
+            "value": 1472,
+            "unit": "B/op",
+            "extra": "337650 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMiddleware - allocs/op",
+            "value": 11,
+            "unit": "allocs/op",
+            "extra": "337650 times\n4 procs"
           }
         ]
       }
