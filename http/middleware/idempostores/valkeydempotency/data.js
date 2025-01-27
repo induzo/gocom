9f5dc66c788d56318f70015e5d77f5f6ea9723be
github-action-benchmark <github@users.noreply.github.com>
2025-01-27 15:57:58 +0000

    add Benchmark (go) benchmark result for b53b2184bead7ee0a4b79e2c83cfc454f7d7f097

---
+++ http/middleware/idempostores/valkeydempotency/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1737993478010,
+  "repoUrl": "https://github.com/induzo/gocom",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b53b2184bead7ee0a4b79e2c83cfc454f7d7f097",
+          "message": "feat: induzo/http/middleware/idempostores/valkeydempotency\n\nhttp/middleware/idempostores/valkeydempotency",
+          "timestamp": "2025-01-27T23:57:08+08:00",
+          "tree_id": "7088bc1883298039a8e513cea4e2b7bd83cafe39",
+          "url": "https://github.com/induzo/gocom/commit/b53b2184bead7ee0a4b79e2c83cfc454f7d7f097"
+        },
+        "date": 1737993477392,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStoreStoreResponse",
+            "value": 462006,
+            "unit": "ns/op\t    2639 B/op\t      40 allocs/op",
+            "extra": "2510 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStoreStoreResponse - ns/op",
+            "value": 462006,
+            "unit": "ns/op",
+            "extra": "2510 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStoreStoreResponse - B/op",
+            "value": 2639,
+            "unit": "B/op",
+            "extra": "2510 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStoreStoreResponse - allocs/op",
+            "value": 40,
+            "unit": "allocs/op",
+            "extra": "2510 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
