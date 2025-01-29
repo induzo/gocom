5d941c73769d5d1845db96b9dd305d7668438797
github-action-benchmark <github@users.noreply.github.com>
2025-01-29 11:11:58 +0000

    add Benchmark (go) benchmark result for 5c1089eb2d99efab46e487375541d8deb0b9657b

--- http/middleware/valkeydempotency/data.js
+++ http/middleware/valkeydempotency/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738149032216,
+  "lastUpdate": 1738149118984,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -48,6 +48,54 @@ window.BENCHMARK_DATA = {
             "value": 40,
             "unit": "allocs/op",
             "extra": "2623 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "Vincent Serpoul",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "5c1089eb2d99efab46e487375541d8deb0b9657b",
+          "message": "docs: update README",
+          "timestamp": "2025-01-29T19:11:02+08:00",
+          "tree_id": "52b2c9b43e1174b010b33aa148723a9c6411bafa",
+          "url": "https://github.com/induzo/gocom/commit/5c1089eb2d99efab46e487375541d8deb0b9657b"
+        },
+        "date": 1738149118558,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkStoreStoreResponse",
+            "value": 460924,
+            "unit": "ns/op\t    2636 B/op\t      40 allocs/op",
+            "extra": "2490 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStoreStoreResponse - ns/op",
+            "value": 460924,
+            "unit": "ns/op",
+            "extra": "2490 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStoreStoreResponse - B/op",
+            "value": 2636,
+            "unit": "B/op",
+            "extra": "2490 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkStoreStoreResponse - allocs/op",
+            "value": 40,
+            "unit": "allocs/op",
+            "extra": "2490 times\n4 procs"
           }
         ]
       }
