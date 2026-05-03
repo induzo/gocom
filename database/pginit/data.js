9d4931b9adb0758ae7d19bc8465d5b6ed90db491
github-action-benchmark <github@users.noreply.github.com>
2026-05-03 10:40:05 +0000

    add Benchmark (go) benchmark result for 086ffb9aa4f9e9a3179409f559fc0032edff1482

--- database/pginit/data.js
+++ database/pginit/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770806432781,
+  "lastUpdate": 1777804805960,
   "repoUrl": "https://github.com/induzo/gocom",
   "entries": {
     "Benchmark": [
@@ -4194,6 +4194,54 @@ window.BENCHMARK_DATA = {
             "value": 299,
             "unit": "allocs/op",
             "extra": "666 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vincent@serpoul.com",
+            "name": "VincentS",
+            "username": "vincentserpoul"
+          },
+          "committer": {
+            "email": "vincent@serpoul.com",
+            "name": "VincentS",
+            "username": "vincentserpoul"
+          },
+          "distinct": true,
+          "id": "086ffb9aa4f9e9a3179409f559fc0032edff1482",
+          "message": "ci: update trivy",
+          "timestamp": "2026-05-03T12:38:38+02:00",
+          "tree_id": "ce16b46f84a7c23b83e44a60148e323d24f54fec",
+          "url": "https://github.com/induzo/gocom/commit/086ffb9aa4f9e9a3179409f559fc0032edff1482"
+        },
+        "date": 1777804804907,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkConnPool",
+            "value": 2637620,
+            "unit": "ns/op\t   44115 B/op\t     308 allocs/op",
+            "extra": "472 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkConnPool - ns/op",
+            "value": 2637620,
+            "unit": "ns/op",
+            "extra": "472 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkConnPool - B/op",
+            "value": 44115,
+            "unit": "B/op",
+            "extra": "472 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkConnPool - allocs/op",
+            "value": 308,
+            "unit": "allocs/op",
+            "extra": "472 times\n4 procs"
           }
         ]
       }
